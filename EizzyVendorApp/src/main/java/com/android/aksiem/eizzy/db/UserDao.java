c32a38b456bf54a9336c08d7760d393a24b3dbb2
napendersingh <napender.singh@gmail.com>
2018-03-30 19:31:02 +0530

    Changed the root folder app from app to EizzyVendorApp

--- EizzyVendorApp/src/main/java/com/android/aksiem/eizzy/db/UserDao.java
+++ EizzyVendorApp/src/main/java/com/android/aksiem/eizzy/db/UserDao.java
@@ -14,4 +14,24 @@
  * limitations under the License.
  */
 
-include ':EizzyVendorApp'
+package com.android.aksiem.eizzy.db;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+
+import com.android.aksiem.eizzy.vo.User;
+
+/**
+ * Interface for database access for User related operations.
+ */
+@Dao
+public interface UserDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(User user);
+
+    @Query("SELECT * FROM user WHERE login = :login")
+    LiveData<User> findByLogin(String login);
+}
--- EizzyVendorApp/src/test/java/com/android/aksiem/eizzy/util/ApiUtil.java
+++ EizzyVendorApp/src/test/java/com/android/aksiem/eizzy/util/ApiUtil.java
@@ -14,4 +14,22 @@
  * limitations under the License.
  */
 
-include ':EizzyVendorApp'
+package com.android.aksiem.eizzy.util;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+
+import com.android.aksiem.eizzy.api.ApiResponse;
+
+import retrofit2.Response;
+
+public class ApiUtil {
+    public static <T> LiveData<ApiResponse<T>> successCall(T data) {
+        return createCall(Response.success(data));
+    }
+    public static <T> LiveData<ApiResponse<T>> createCall(Response<T> response) {
+        MutableLiveData<ApiResponse<T>> data = new MutableLiveData<>();
+        data.setValue(new ApiResponse<>(response));
+        return data;
+    }
+}
