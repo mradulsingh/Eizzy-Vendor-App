20515eb115be830a30ee28a9f33365c9517f73ff
napendersingh <napender.singh@gmail.com>
2018-06-10 00:51:02 +0530

    Fixed issue with Order Items Fragment layout

--- EizzyVendorApp/src/main/java/com/android/aksiem/eizzy/ui/order/OrderItemsFragment.java
+++ EizzyVendorApp/src/main/java/com/android/aksiem/eizzy/ui/order/OrderItemsFragment.java
@@ -8,6 +8,8 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.view.ViewCompat;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -108,7 +110,9 @@ public class OrderItemsFragment extends NavigationFragment {
                     navigationController.navigateToOrderDetailsFragment(orderItem);
                 });
         this.adapter = new AutoClearedValue<>(this, adapter);
-        binding.get().orderList.setAdapter(adapter);
+        RecyclerView recyclerView = binding.get().orderList;
+        ViewCompat.setNestedScrollingEnabled(recyclerView, false);
+        recyclerView.setAdapter(adapter);
     }
 
     private void initOrdersList() {
